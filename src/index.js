7c7df107e9f3ae6c06b5ed00dea6f70d1721107a
Madison Shimizu <mhshimizu@outlook.com>
2021-11-09 21:01:19 -1000

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDo(e.target['new-task-description']['value'])
+  })
+})
+
+function buildToDo (todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  p.textContent = `${todo} `  
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+  btn.addEventListener('click', handleDelete)
+  } 
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
